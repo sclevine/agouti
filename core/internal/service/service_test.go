44236ff99a2de486cffed3b510cea8b150470066
Stephen Levine <stephen.levine@gmail.com>
2014-10-16 20:41:10 -0700

    Increase another phantomjs timeout to address flakey travis spec

--- core/internal/service/service_test.go
+++ core/internal/service/service_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Service", func() {
 		url = "http://" + address
 		service = &Service{
 			URL:     url,
-			Timeout: 3 * time.Second,
+			Timeout: 5 * time.Second,
 			Command: []string{"phantomjs", "--webdriver=" + address},
 		}
 	})
