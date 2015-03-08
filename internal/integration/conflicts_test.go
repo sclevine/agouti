28bb2868658c4649caac3b303a4a8a97c590d809
Stephen Levine <stephen.levine@gmail.com>
2015-03-08 10:22:16 -0700

    Remove dsl package from conflicts test to fix build errors

--- internal/integration/conflicts_test.go
+++ internal/integration/conflicts_test.go
@@ -4,12 +4,11 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/sclevine/agouti"
-	. "github.com/sclevine/agouti/dsl"
 	. "github.com/sclevine/agouti/matchers"
 )
 
-var _ = Feature("Conflicts", func() {
-	It("should allow dot-importing matchers, dsl, ginkgo, and gomega", func() {
+var _ = Describe("Conflicts", func() {
+	It("should allow importing agouti while dot-importing matchers, ginkgo, and gomega", func() {
 		Expect(agouti.Capabilities{}).To(Equal(agouti.Capabilities{}))
 		Expect(HaveTitle("title")).To(Equal(HaveTitle("title")))
 	})
